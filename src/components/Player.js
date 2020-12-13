8b84403cdc15a016d924b1cecf5b61d9013d3460
BoCoder <agarrido84@gmail.com>
2020-12-12 19:22:44 -0500

    time on slider ends

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     // ref
@@ -15,12 +15,27 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             setIsPlaying(!isPlaying)
         }
     }
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime: current, duration: duration})
+    }
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60 ) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        )
+    }
+    // state of the song
+    const [songInfo, setSongInfo] = useState({
+        currentTime: null,
+        duration: null,
+    })
     return(
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
                 <input type="range"/>
-                <p>End Time</p>
+                <p> {getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
             <FontAwesomeIcon className="skip-back" size='2x' icon={faAngleLeft} />
@@ -29,7 +44,10 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             size='2x' icon={faPlay} />
             <FontAwesomeIcon className="skip-forward" size='2x' icon={faAngleRight} />
             </div>
-            <audio ref={audioRef} src={currentSong.audio}></audio>
+            <audio 
+            onLoadedMetadata={timeUpdateHandler}
+            onTimeUpdate={timeUpdateHandler}
+            ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 }
